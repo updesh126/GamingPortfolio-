70ac885cf0adc2da7d910c044bb7e75a6ac784de
Updesh Pr. Singh <updesh071@gmail.com>
2022-10-17 19:04:11 +0530

    login php

---
+++ Assets/Backend/GetWebReq.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class GetWebReq : MonoBehaviour
+{
+    void Start()
+    {
+        // A correct website page.
+        StartCoroutine(GetData("http://localhost/backend/GetDate.php"));
+        StartCoroutine(GetUser("http://localhost/backend/GetUser.php"));
+         
+        // A non-existing page.
+        StartCoroutine(GetData("https://error.html"));
+    }
+
+    IEnumerator GetData(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+            }
+        }
+    }
+
+    IEnumerator GetUser(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+            }
+        }
+    }
+}
